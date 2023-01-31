6c9dc7d929c1fe4f687706897b74a85e9bcb120a
Marcin <97787455+TheScratchy@users.noreply.github.com>
2023-01-31 20:35:32 +0100

    Add files via upload

---
+++ View/GameBoard_Window.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Chess_Learning_Application;
+
+namespace Wpf_GUI.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GameBoard_Window.xaml
+    /// </summary>
+    public partial class GameBoard_Window : Window
+    {
+        public GameBoard_Window(bool playVsAI, string aiDiff, Chessboard.FigureColor aiColor, bool isTimeEnabled, int time, int timeInc)
+        {
+            InitializeComponent();
+            DataContext = new ViewModelChessboard( playVsAI,  aiDiff,  aiColor,  isTimeEnabled,  time,  timeInc);
+        }
+    }
+}
